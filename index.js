16c44d233becbe39b6790459b24783739cf6a844
Mikulzski <gmmikulski@gmail.com>
2020-02-17 13:06:12 -0500

    added dog age calculator

---
+++ index.js
@@ -0,0 +1,12 @@
+function calculateDogAge (humanAge)
+{
+    if(humanAge >=10)
+    {
+        humanAge *= 7;
+        return humanAge;
+    }
+    else
+    {
+        console.log('You are too young to be calculating your dog age!')
+    }
+}
